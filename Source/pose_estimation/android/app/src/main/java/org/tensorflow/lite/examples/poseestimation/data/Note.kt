f74ac2c28b5aa8c2b1e2cb1c0ae4e9c70d5f2a36
paxcho1 <paxcho1@naver.com>
2021-10-15 12:02:28 +0900

    note data class

---
+++ Source/pose_estimation/android/app/src/main/java/org/tensorflow/lite/examples/poseestimation/data/Note.kt
@@ -0,0 +1,173 @@
+package org.tensorflow.lite.examples.poseestimation.data
+
+data class Note(){
+    var C0 = 16.35
+    var Cs0 = 17.32
+    var Df0 = 17.32
+    var D0 = 18.35
+    var Ds0 = 19.45
+    var Ef0 = 19.45
+    var E0 = 20.60
+    var F0 = 21.83
+    var Fs0 = 23.12
+    var Gf0 = 23.12
+    var G0 = 24.50
+    var Gs0 = 25.96
+    var Ab0 = 25.96
+    var A0 = 27.50
+    var As0 = 29.14
+    var Bf0 = 29.14
+    var B0 = 30.87
+
+    /*                            */
+    var C1 = 32.70
+    var Cs1 = 34.65
+    var Df1 = 34.65
+    var D1 = 36.71
+    var Ds1 = 38.89
+    var Ef1 = 38.89
+    var E1 = 41.20
+    var F1 = 43.65
+    var Fs1 = 46.25
+    var Gf1 = 46.25
+    var G1 = 49.00
+    var Gs1 = 51.91
+    var Ab1 = 51.91
+    var A1 = 55.00
+    var As1 = 58.27
+    var Bf1 = 58.27
+    var B1 = 61.74
+
+    /*                            */
+    var C2 = 65.41
+    var Cs2 = 69.30
+    var Df2 = 69.30
+    var D2 = 73.42
+    var Ds2 = 77.78
+    var Ef2 = 77.78
+    var E2 = 82.41
+    var F2 = 87.31
+    var Fs2 = 92.50
+    var Gf2 = 92.50
+    var G2 = 98.00
+    var Gs2 = 103.83
+    var Ab2 = 103.83
+    var A2 = 110.00
+    var As2 = 116.54
+    var Bf2 = 116.54
+    var B2 = 123.47
+
+    /*                            */
+    var C3 = 130.81
+    var Cs3 = 138.59
+    var Df3 = 138.59
+    var D3 = 146.83
+    var Ds3 = 155.56
+    var Ef3 = 155.56
+    var E3 = 164.81
+    var F3 = 174.61
+    var Fs3 = 185.00
+    var Gf3 = 185.00
+    var G3 = 196.00
+    var Gs3 = 207.65
+    var Ab3 = 207.65
+    var A3 = 220.00
+    var As3 = 233.08
+    var Bf3 = 233.08
+    var B3 = 246.94
+
+    /*                            */
+    var C4 = 261.63
+    var Cs4 = 277.18
+    var Df4 = 277.18
+    var D4 = 293.66
+    var Ds4 = 311.13
+    var Ef4 = 311.13
+    var E4 = 329.63
+    var F4 = 349.23
+    var Fs4 = 369.99
+    var Gf4 = 369.99
+    var G4 = 392.00
+    var Gs4 = 415.30
+    var Ab4 = 415.30
+    var A4 = 440.00
+    var As4 = 466.16
+    var Bf4 = 466.16
+    var B4 = 493.88
+
+    /*                            */
+    var C5 = 523.25
+    var Cs5 = 554.37
+    var Df5 = 554.37
+    var D5 = 587.33
+    var Ds5 = 622.25
+    var Ef5 = 622.25
+    var E5 = 659.25
+    var F5 = 698.46
+    var Fs5 = 739.99
+    var Gf5 = 739.99
+    var G5 = 783.99
+    var Gs5 = 830.61
+    var Ab5 = 830.61
+    var A5 = 880.00
+    var As5 = 932.33
+    var Bf5 = 932.33
+    var B5 = 987.77
+
+    /*                            */
+    var C6 = 1046.50
+    var Cs6 = 1108.73
+    var Df6 = 1108.73
+    var D6 = 1174.66
+    var Ds6 = 1244.51
+    var Ef6 = 1244.51
+    var E6 = 1318.51
+    var F6 = 1396.91
+    var Fs6 = 1479.98
+    var Gf6 = 1479.98
+    var G6 = 1567.98
+    var Gs6 = 1661.22
+    var Ab6 = 1661.22
+    var A6 = 1760.00
+    var As6 = 1864.66
+    var Bf6 = 1864.66
+    var B6 = 1975.53
+
+    /*                            */
+    var C7 = 2093.00
+    var Cs7 = 2217.46
+    var Df7 = 2217.46
+    var D7 = 2349.32
+    var Ds7 = 2489.02
+    var Ef7 = 2489.02
+    var E7 = 2637.02
+    var F7 = 2793.83
+    var Fs7 = 2959.96
+    var Gf7 = 2959.96
+    var G7 = 3135.96
+    var Gs7 = 3322.44
+    var Ab7 = 3322.44
+    var A7 = 3520.00
+    var As7 = 3729.31
+    var Bf7 = 3729.31
+    var B7 = 3951.07
+
+    /*                            */
+    var C8 = 4186.01
+    var Cs8 = 4434.92
+    var Df8 = 4434.92
+    var D8 = 4698.63
+    var Ds8 = 4978.03
+    var Ef8 = 4978.03
+    var E8 = 5274.04
+    var F8 = 5587.65
+    var Fs8 = 5919.91
+    var Gf8 = 5919.91
+    var G8 = 6271.93
+    var Gs8 = 6644.88
+    var Ab8 = 6644.88
+    var A8 = 7040.00
+    var As8 = 7458.62
+    var Bf8 = 7458.62
+    var B8 = 7902.13
+}
